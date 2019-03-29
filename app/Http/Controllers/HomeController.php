441a20af586ca0ff6dfe71a9e9d2ce7c972f2f14
Tinashe <tinashe@flintstudio.com>
2019-03-29 15:30:54 +0200

    Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
 
 class HomeController extends Controller
 {
@@ -23,9 +24,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
-    }
+        $chart_options = [
+            'chart_title' => 'Products by Month',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Product',
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'day',
+            'chart_type' => 'line',
+            'filter_field' => 'created_at',
+            'filter_days' => 10, 
+        ];
+    
+        $chart1 = new LaravelChart($chart_options);
 
+        
+
+        return view('home',compact('chart1'));
+    }
     public function accountSettings()
     {
         return view('settings.account');
